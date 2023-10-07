a71904ec7b03bba773e1143f974282c2345f9ea3
Lital <lital.h.ben.baruch@gmail.com>
2023-10-07 16:17:21 +0300

    Added docstrings and fixed a bug in the rearrange_corners function.

--- SmartDocScanner.py
+++ SmartDocScanner.py
@@ -16,6 +16,15 @@ BOUNDING_BOX_AREA_THRESHOLD = 5000
 
 
 def preprocess_image(image):
+    """
+        Preprocesses the input image for paper contour detection.
+
+        Args:
+            image (numpy.ndarray): The input image.
+
+        Returns:
+            numpy.ndarray: The preprocessed image.
+        """
     # Convert to grayscale
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply Gaussian blur
@@ -29,8 +38,18 @@ def preprocess_image(image):
 
 
 def find_and_draw_largest_contour(image, image_with_contours):
+    """
+        Finds and draws the largest paper contour on the input image.
+
+        Args:
+            image (numpy.ndarray): The input image.
+            image_with_contours (numpy.ndarray): The image on which contours will be drawn.
+
+        Returns:
+            numpy.ndarray: The largest paper contour.
+        """
     contour_color = (255, 0, 0)
-    contour_thickness = 20
+    contour_thickness = 5
     is_closed_curve = True
     approximation_resolution = 0.02
     max_area_contour = np.array([])
@@ -51,31 +70,91 @@ def find_and_draw_largest_contour(image, image_with_contours):
     return max_area_contour
 
 
-def rearrange_corners(approx):
+def contour_centroid(approx):
+    M = cv2.moments(approx)
     points = approx.reshape((4, 2))
-    new_approx = np.zeros((4, 1, 2), np.int32)
-    corner_sum = points.sum(1)
 
-    new_approx[0] = points[np.argmin(corner_sum)]
-    new_approx[3] = points[np.argmax(corner_sum)]
-    corner_diff = np.diff(points, axis=1)
-    new_approx[1] = points[np.argmin(corner_diff)]
-    new_approx[2] = points[np.argmax(corner_diff)]
+    if M["m00"] != 0:
+        center_x = int(M["m10"] / M["m00"])
+        center_y = int(M["m01"] / M["m00"])
+    else:
+        # Handle the case where the moments are undefined
+        center_x = int(np.mean(points[:, 0]))
+        center_y = int(np.mean(points[:, 1]))
+    return center_x, center_y
 
-    return new_approx
+
+def rearrange_corners(approx):
+    """
+    Rearranges the corners of a paper contour to ensure a consistent order.
+
+    Args:
+          approx (numpy.ndarray): The paper contour, represented as a NumPy array of shape (4, 1, 2).
+
+    Returns:
+        numpy.ndarray: The rearranged paper contour, with corners sorted in a consistent order.
+    """
+    # Reshape the input paper contour to a 4x2 array for processing
+    points = approx.reshape((4, 2))
+
+    # Calculate the centroid of the paper contour
+    center_x, center_y = contour_centroid(approx)
+
+    # Calculate the angles of each corner relative to the centroid
+    angles = np.arctan2(points[:, 1] - center_y, points[:, 0] - center_x)
+
+    # Sort the corner points based on their angles in a clockwise order
+    sorted_indices = np.argsort(angles)
+    sorted_corners = points[sorted_indices]
+
+    # Calculate distances 'a' and 'b' between specific corner points - adjusting paper layout
+    # Rearrange the sorted corners to match the desired order
+    # dst_points =[[0, 0], [w, 0], [w, h], [0, h]]--> A  B  C  D
+    # (0,0)-->(w,0) = b
+    # (0,0)-->(0,h) = a
+    b = np.sqrt((sorted_corners[1][0] - sorted_corners[0][0]) ** 2 + (sorted_corners[1][1] - sorted_corners[0][1]) ** 2)
+    a = np.sqrt((sorted_corners[3][0] - sorted_corners[0][0]) ** 2 + (sorted_corners[3][1] - sorted_corners[0][1]) ** 2)
+
+    if a * 1.2 < b:  # correct the layout of the paper
+        approx[[0]] = sorted_corners[3]
+        approx[[1]] = sorted_corners[0]
+        approx[[2]] = sorted_corners[1]
+        approx[[3]] = sorted_corners[2]
+    if a * 1.2 > b:
+        sorted_corners = sorted_corners.reshape((4, 1, 2))
+        approx = sorted_corners
+
+    return approx
 
 
 def get_warped_image(image, approx):
-    approx = rearrange_corners(approx)
+    """
+        Warps the input image using the provided paper contour.
+
+        Args:
+            image (numpy.ndarray): The input image.
+            approx (numpy.ndarray): The paper contour.
+
+        Returns:
+            numpy.ndarray: The warped image.
+        """
     paper_width, paper_height = 480, 640
+    approx = rearrange_corners(approx)
+
     src_points = np.float32(approx)
-    dst_points = np.float32([[0, 0], [paper_width, 0], [0, paper_height], [paper_width, paper_height]])
+    # dst_points =[[0, 0], [w, 0], [w, h], [0, h]]--> A  B  C  D
+    dst_points = np.float32([[0, 0], [paper_width, 0], [paper_width, paper_height], [0, paper_height]])
     matrix = cv2.getPerspectiveTransform(src_points, dst_points)
     warped_image = cv2.warpPerspective(image, matrix, (paper_width, paper_height))
     return warped_image
 
 
-def check_image_dimensions(image):
+def check_image_dimensions(image):  # TODO
+    """
+    Checks the dimensions of the input image and provides information.
+    Args:
+        image (numpy.ndarray): The input image.
+    """
     if len(image.shape) == 2:
         print("The image is grayscale (1D).")
         image = cv2.merge((image, image, image))
@@ -108,11 +187,10 @@ def main():
         # Preprocess the frame
         processed_frame = preprocess_image(frame)
         max_area_contour = find_and_draw_largest_contour(processed_frame, image_with_contours)
-        print(max_area_contour)
 
         if np.any(max_area_contour):
             # The array is not empty
-            print("Array is not empty")
+            print("max_area_contour", max_area_contour)
             warped_image = get_warped_image(frame, max_area_contour)
         else:
             # The array is empty
